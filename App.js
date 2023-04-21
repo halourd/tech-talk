9ea4c5616eb3fb57d7a534931f683e252ffab153
Harold Gallego <halord.py@gmail.com>
2023-04-22 01:29:59 +0800

    add essential files and folders

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require('./assets/logo/logo.png')} style={{width: 200, height: 200, marginVertical: 10}}/>
+      <Text style={{fontSize: 28, fontWeight: 600}}>Tech Talk</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ api/api.js
@@ -0,0 +1,11 @@
+import {Request} from '../helper/api'
+
+const request = new Request;
+
+export class requestAPI {
+
+//   register(user_data){
+//     return request.post('register', {...user_data})
+//   }
+
+}
